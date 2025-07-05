6e199c1e7c0ae2f209e9809b8c2e3baa499a331e
ManojVattikuti982 <manojvattikuti2000@gmail.com>
2025-07-05 21:00:32 +0530

    arithmetic operators.js

---
+++ arithmetic.js
@@ -0,0 +1,51 @@
+const x = 12;
+const y = 3;
+//output: 15 (Addition)
+// 15
+// 9
+// 36
+// 4
+// 0
+// 1728
+
+// const x = "12";
+// const y = "3";  
+// output: "123" (String concatenation)
+// 123
+// 9
+// 36
+// 4
+// 0
+// 1728
+
+// const x = 12;
+// const y = "3";  
+// output: "123" (String concatenation)
+// 123
+// 9
+// 36
+// 4
+// 0
+// 1728
+
+// const x = "mango";
+// const y = "apple";
+// output: NaN (Not a Number)
+// mangoapple
+// NaN
+// NaN
+// NaN
+// NaN
+// NaN
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x % y);
+
+console.log(x ** y); // Exponentiation operator 
